17e0be66b12ec19c08cc15d36ccedd23e97d6230
Dashazem <dashazemska@gmail.com>
2024-05-07 20:01:40 +0200

    first commit

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -23,22 +23,22 @@ const getMealTime = () => {
   const currentTime = moment().format('HH:mm');
 
   if (moment(currentTime, 'HH:mm').isBetween(moment('00:00', 'HH:mm'), moment('12:00', 'HH:mm'))) {
-      return 'breakfast';
+    return 'breakfast';
   } else if (moment(currentTime, 'HH:mm').isBetween(moment('12:00', 'HH:mm'), moment('18:00', 'HH:mm'))) {
-      return 'lunch';
+    return 'lunch';
   } else {
-      return 'dinner';
+    return 'dinner';
   }
 }
 
 const userInput = prompt(`Welcome to our menu! Please choose a category:\n${Object.keys(menu.mainMenu).map(key => `• ${key}`).join('\n')}`, `${getMealTime()}`);
 
-if(userInput.toLowerCase() && menu.mainMenu[userInput.toLowerCase()]){
+if (userInput.toLowerCase() && menu.mainMenu[userInput.toLowerCase()]) {
   const subMenu = menu.mainMenu[userInput.toLowerCase()];
 
   const entree = prompt(`This is our ${userInput.toLowerCase()} menu.\nEntrees:\n${Object.keys(subMenu).map(key => `• ${key} - $${subMenu[key].price}`).join('\n')}\nSide dishes:\n${Object.keys(menu.sidesMenu).map(key => `• ${key} - $${menu.sidesMenu[key].price}`).join('\n')}\n\nTo start your order, please choose an entree for your ${userInput}.`);
 
-  if(entree.toLowerCase() && subMenu[entree.toLowerCase()]){
+  if (entree.toLowerCase() && subMenu[entree.toLowerCase()]) {
     const selectedEntree = subMenu[entree.toLowerCase()];
 
     if (selectedEntree) {
@@ -68,7 +68,7 @@ if(userInput.toLowerCase() && menu.mainMenu[userInput.toLowerCase()]){
     
     const firstSide = prompt(`Please choose a First side dish:\n${Object.keys(menu.sidesMenu).map(key => `• ${key} - $${menu.sidesMenu[key].price}`).join('\n')}`);
 
-    if(firstSide.toLowerCase() && menu.sidesMenu[firstSide.toLowerCase()]){
+    if (firstSide.toLowerCase() && menu.sidesMenu[firstSide.toLowerCase()]) {
       const selectedFirstSide = menu.sidesMenu[firstSide.toLowerCase()];
       alert(`You have selected ${firstSide.toLowerCase()} with price: $${selectedFirstSide.price}\n${selectedFirstSide.comment}\n${getRandomComment()}`);
 
@@ -76,7 +76,7 @@ if(userInput.toLowerCase() && menu.mainMenu[userInput.toLowerCase()]){
 
       const secondSide = prompt(`Please choose a Second side dish:\n${Object.keys(menu.sidesMenu).map(key => `• ${key} - $${menu.sidesMenu[key].price}`).join('\n')}`);
 
-      if(secondSide.toLowerCase() && menu.sidesMenu[secondSide.toLowerCase()]){
+      if (secondSide.toLowerCase() && menu.sidesMenu[secondSide.toLowerCase()]) {
         const selectedSecondSide = menu.sidesMenu[secondSide.toLowerCase()];
 
         alert(`You have selected ${secondSide.toLowerCase()} with price: $${selectedSecondSide.price}\n${selectedSecondSide.comment}\n${getRandomComment()}`);
@@ -84,15 +84,15 @@ if(userInput.toLowerCase() && menu.mainMenu[userInput.toLowerCase()]){
         total += selectedSecondSide.price;
         alert(`Total price for your order: ${entree.toLowerCase()} ${chosenType.toLowerCase()}, ${firstSide.toLowerCase()}, ${secondSide.toLowerCase()} - $${total}`);
 
-      }else{
+      } else {
         alert("Invalid selection of second side dish. The specified name of the dish is not on the menu. Make sure you enter the name without spelling mistakes. Please try again.");
       }
-    }else{
+    } else {
       alert("Invalid selection of first side dish. The specified name of the dish is not on the menu. Make sure you enter the name without spelling mistakes. Please try again.");
     }  
-  }else{
+  } else {
     alert("Invalid selection of entree. The specified name of the dish is not on the menu. Make sure you enter the name without spelling mistakes. Please try again.");
   }
-}else{
+} else {
   alert("Invalid category selected. Make sure you enter the name without spelling mistakes. Please try again.");
 }
